a9e90ee4da5792714743a77760c21a9e4afc2ae3
Bavoen <bavovh@hotmail.com>
2021-02-19 19:47:06 +0100

    Removed legacy code

--- csharpcore/GildedRose.cs
+++ csharpcore/GildedRose.cs
@@ -5,11 +5,6 @@ namespace csharpcore
 {
     public class GildedRose
     {
-        private const string AGED_BRIE = "Aged Brie";
-        private const string BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-        private const string SULFURAS = "Sulfuras, Hand of Ragnaros";
-        private const string CONJURED = "Conjured Mana Cake";
-
         readonly IList<Item> Items;
         internal ItemQualityUpdaterFactory ItemQualityUpdaterFactory { get; }
 
@@ -23,88 +18,8 @@ namespace csharpcore
         {
             foreach (Item item in Items)
             {
-                if(item.Name != CONJURED)
-                {
-                    var qualityUpdater = ItemQualityUpdaterFactory.GetQualityUpdater(item);
-                    qualityUpdater.UpdateQuality(item);
-                }
-                else
-                {
-                    LegacyUpdateQuality(item);
-                }
-            }
-        }
-
-        public void LegacyUpdateQuality(Item item)
-        {
-            if (item.Name != AGED_BRIE && item.Name != BACKSTAGE_PASSES)
-            {
-                if (item.Quality > 0)
-                {
-                    if (item.Name != SULFURAS)
-                    {
-                        item.Quality = item.Quality - 1;
-                    }
-                }
-            }
-            else
-            {
-                if (item.Quality < 50)
-                {
-                    item.Quality = item.Quality + 1;
-
-                    if (item.Name == BACKSTAGE_PASSES)
-                    {
-                        if (item.SellIn < 11)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-
-                        if (item.SellIn < 6)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (item.Name != SULFURAS)
-            {
-                item.SellIn = item.SellIn - 1;
-            }
-
-            if (item.SellIn < 0)
-            {
-                if (item.Name != AGED_BRIE)
-                {
-                    if (item.Name != BACKSTAGE_PASSES)
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name != SULFURAS)
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        item.Quality = item.Quality - item.Quality;
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-                    }
-                }
+                var qualityUpdater = ItemQualityUpdaterFactory.GetQualityUpdater(item);
+                qualityUpdater.UpdateQuality(item);
             }
         }
     }
